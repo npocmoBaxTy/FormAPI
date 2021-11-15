087ccc038909f1880855782a6a0b06738e7c242b
npocmoBaxTy <baxtyzinatdinov@gmail.com>
2021-11-15 23:49:41 +0500

    changed style

--- js/main.js
+++ js/main.js
@@ -1,131 +1,146 @@
-const loginReg = document.querySelector('.loginReg')
-const passReg = document.querySelector('.passReg')
-const regBtn = document.querySelector('.regBtn')
-const regEmail = document.querySelector('.regEmail')
+var instance = M.Tabs.init(document.querySelector('.tabs'), {
+    duration: 1000
+})
 
 
-const loginSign = document.querySelector('.loginSign')
-const passSign = document.querySelector('.passSign')
-const signBtn = document.querySelector('.signBtn')
+const signUpBtn = document.querySelector('.sign__up-btn')
+const signInBtn = document.querySelector('.sign__in-btn')
 
+const upInputs = document.querySelectorAll('#sign-up input')
+const inINputs = document.querySelectorAll('#sign-in input')
+
+//Data
+let url = 'https://618e8c7050e24d0017ce1360.mockapi.io/api/v1/users'
+async function getData() {
+    const res = await fetch(url)
+    const data = await res.json()
+    return data
+}
+
+
+
+function inValid() {
+    const fname = document.forms['form']['first_name'].value
+    if (fname === "") {
+        document.forms['form']['first_name'].classList.add('invalid')
+        signUpBtn.classList.add('disabled')
+        return false
+    }
+    const lname = document.forms['form']['last_name'].value
+    if (lname === "") {
+        signUpBtn.classList.add('disabled')
+        document.forms['form']['last_name'].classList.add('invalid')
+        return false
+    }
+    const email = document.forms['form']['email'].value
+    if (!email.includes('@') || email === "") {
+        signUpBtn.classList.add('disabled')
+        document.forms['form']['email'].classList.add('invalid')
+        return false
+    } else {
+        document.forms['form']['email'].classList.remove('invalid')
+        document.forms['form']['email'].classList.add('valid')
+        signUpBtn.classList.remove('disabled')
+    }
+
+    const login = document.forms['form']['login'].value
+    if (login === "" || login.length < 5) {
+        signUpBtn.classList.add('disabled')
+        document.forms['form']['login'].classList.add('invalid')
+        return false
+    }
+    const pass = document.forms['form']['password'].value
+    if (pass === "" || pass.length < 5) {
+        signUpBtn.classList.add('disabled')
+        document.forms['form']['password'].classList.add('invalid')
+        return false
+    }
+    signUpBtn.classList.remove('disabled')
+    return true
+}
+
+upInputs.forEach(input => {
+    input.addEventListener('blur', e => {
+        inValid()
+    })
+})
+
+
+signUpBtn.addEventListener('click', postUser)
+
+async function postUser() {
+    const user = {
+        fname: document.forms['form']['first_name'].value.toLowerCase(),
+        lname: document.forms['form']['last_name'].value.toLowerCase(),
+        email: document.forms['form']['email'].value.toLowerCase(),
+        login: document.forms['form']['login'].value.toLowerCase(),
+        pass: document.forms['form']['password'].value.toLowerCase()
+    }
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json',
+                'charset': 'utf-8'
+            }
+        });
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+    upInputs.forEach(input => {
+        input.value = ""
+        input.classList.remove('valid')
+    })
+}
+
+
+
+
+
+const logUp = document.getElementById('sign__in-email')
+const passUp = document.getElementById('sign__in-password')
 
 const successful = document.querySelector('.successful')
 const unsuccessful = document.querySelector('.unsuccessful')
 
+let login = false;
+async function getUser() {
+    const data = await getData()
 
-const showPass = document.querySelectorAll('.show__pass')
+    data.forEach((user, index) => {
 
-showPass.forEach(item=> {
-	item.addEventListener('click',e=> {
-	if (e.target.previousElementSibling.getAttribute('type') == 'password') {
-		e.target.classList.add('active');
-		e.target.previousElementSibling.setAttribute('type', 'text');
-	} else {
-		e.target.classList.remove('active');
-		e.target.previousElementSibling.setAttribute('type', 'password');
-	}
-})
-})
+        const logValue = logUp.value.toLowerCase()
+        const passValue = passUp.value.toLowerCase()
 
-
-let url = 'https://618e8c7050e24d0017ce1360.mockapi.io/api/v1/users'
-async function getData() {
-	const res = await fetch(url)
-	const data = await res.json()
-	return data
-}
-
-regBtn.addEventListener('click',postUser)
-
-async function postUser () {
-
-	const userLogin = loginReg.value.toLowerCase()
-	const userPass = passReg.value.toLowerCase()
-	const userEmail = regEmail.value.toLowerCase()
-	const user = {
-		login:userLogin,
-		pass:userPass,
-		email:userEmail
-	}
-	try {
-  	const response = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: {
-      'Content-Type': 'application/json',
-      'charset' : 'utf-8'
+        if (user.login.toLowerCase() === logValue && user.pass.toLowerCase() === passValue) {
+            login = true
+        }
+    });
+    if (login) {
+        setTimeout(function() {
+            successful.classList.add('active')
+        }, 500)
+        setTimeout(function() {
+            successful.classList.remove('active')
+        }, 3000)
+        console.log(login)
+    } else {
+        unsuccessful.classList.add('active')
+        setTimeout(function() {
+            unsuccessful.classList.remove('active')
+        }, 2000)
+          logUp.value = ""
+          passUp.value = ""
+          passUp.classList.remove('invalid')
+          logUp.classList.remove('valid')
     }
-  });
-	} catch (error) {
-	  console.error('Ошибка:', error);
-	}
-	renderUsersList()
-	loginReg.value = ""
-	passReg.value = ''
-	regEmail.value = ''
-}
-
-	var login = false;
-	async function getUser() {
-	const data = await getData()
-	localStorage.clear()
-
-	data.forEach((user,index)=> {
-
-		const logValue = loginSign.value.toLowerCase()
-		const passValue = passSign.value.toLowerCase()
-		
-		if(user.login.toLowerCase() === logValue && user.pass.toLowerCase() === passValue) {
-			login = true
-		}
-	});
-	if(login) {
-		setTimeout(function(){
-				successful.classList.add('active')
-			},500)
-			setTimeout(function(){
-				successful.classList.remove('active')
-			},3000)
-			console.log(login)
-	}
-	else {
-		  unsuccessful.classList.add('active')
-			setTimeout(function() {
-				unsuccessful.classList.remove('active')
-			},2000)
-			loginSign.value =""
-			passSign.value = ""
-	}
-		loginSign.value =""
-		passSign.value = ""
-		login = false
+    logUp.value = ""
+    passUp.value = ""
+    logUp.classList.remove('valid')
+    passUp.classList.remove('valid')
+    login = false
 
 }
 
-signBtn.addEventListener('click',getUser)
-
-
-const usersList = document.querySelector('.users__list')
-
-
-async function renderUsersList() {
-	const data = await getData()
-	usersList.innerHTML = ""
-	data.forEach(user=> {
-		usersList.insertAdjacentHTML('afterbegin',`
-		<div class="user">
-        <div class="user__login">
-            Login: ${user.login}
-        </div>
-        <div class="user__pass">
-            Password: ${user.pass}
-        </div>
-        <div class="user__pass">
-            Email: ${user.email}
-        </div>
-    </div>
-	`)
-	})
-}
-
-renderUsersList()
+signInBtn.addEventListener('click', getUser)
